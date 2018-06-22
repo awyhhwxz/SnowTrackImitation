75ddb277c6c19a629edfa0af8cf397012a1bc5fb
awyhhwxz <awyhhwxz@gmail.com>
2018-06-22 12:25:45 +0800

    Initial commit

---
+++ Assets/SnowSplatMapDrawer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowSplatMapDrawer : MonoBehaviour {
+
+    public Shader DrawShader;
+    public List<GameObject> DrawPainterList;
+    public RenderTexture RT;
+    private Material _drawMaterial;
+    private RaycastHit _raycastHit;
+
+    [Range(0, 500)]
+    public float Size = 50;
+    [Range(0, 1)]
+    public float Strength = 1;
+    [Range(0, 4)]
+    public float Radius = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _drawMaterial = new Material(DrawShader);
+        _drawMaterial.SetColor("_DrawColor", Color.red);
+        _drawMaterial.SetTexture("_SplatMap", RT);
+
+        var tempRT = RenderTexture.GetTemporary(RT.width, RT.height);
+        var initializeMaterial = new Material(Resources.Load<Shader>("Shader/PureColorDrawShader"));
+        Graphics.Blit(tempRT, RT, initializeMaterial);
+        RenderTexture.ReleaseTemporary(tempRT);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        foreach (var drawPainter in DrawPainterList)
+        {
+            if (Physics.Raycast(drawPainter.transform.position, -drawPainter.transform.up, out _raycastHit, 1, LayerMask.GetMask("Ground")))
+            {
+                _drawMaterial.SetVector("_Coordination", new Vector4(_raycastHit.textureCoord.x, _raycastHit.textureCoord.y, 0, 0));
+                _drawMaterial.SetFloat("_ShaderSize", Size);
+                _drawMaterial.SetFloat("_ShaderStrength", Strength);
+                _drawMaterial.SetFloat("_ShaderRadius", Radius);
+                var tempRT = RenderTexture.GetTemporary(RT.width, RT.height, 0, RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(RT, tempRT);
+                Graphics.Blit(tempRT, RT, _drawMaterial);
+                RenderTexture.ReleaseTemporary(tempRT);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 200, 200), RT, ScaleMode.ScaleToFit, false, 1);
+    }
+}
---
+++ Assets/SnowSplatMapDrawer.shader
@@ -0,0 +1,65 @@
+﻿Shader "Unlit/SnowSplatMapDrawer"
+{
+	Properties
+	{
+		_SplatMap ("Texture", 2D) = "white" {}
+		_DrawColor("Draw Color", Color) = (0, 0, 0, 1)
+		_Coordination("Coordination", Vector) = (0, 0, 0, 0)
+		_ShaderSize("Shader Size", Range(0, 500)) = 50
+		_ShaderStrength("Shader Strength", Range(0, 1)) = 0.5
+		_ShaderRadius("Shader Radius", Range(0, 4)) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _SplatMap;
+			float4 _SplatMap_ST;
+
+			fixed4 _DrawColor;
+			fixed4 _Coordination;
+			fixed _ShaderSize;
+			fixed _ShaderStrength;
+			fixed _ShaderRadius;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _SplatMap);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_SplatMap, i.uv);
+				float draw = pow(saturate(1 - distance(i.uv, _Coordination.xy) * _ShaderRadius), _ShaderSize);
+				fixed4 drawCol = _DrawColor * draw * _ShaderStrength;
+				return saturate(col + drawCol);
+			}
+			ENDCG
+		}
+	}
+}
